c89fdc46356d7290a25cdc244a83d17a4fbe1d9d
debashis-das <2009debashis@gmail.com>
2025-12-20 16:20:08 +0530

    Deb || working forward triton

--- src/transformer/FusedAttention.py
+++ src/transformer/FusedAttention.py
@@ -8,7 +8,7 @@ from triton.tools.tensor_descriptor import TensorDescriptor
 def _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v, 
                              offset_y, dtype: tl.constexpr, start_m, qk_scale,
                              block_m: tl.constexpr, hidden_dim: tl.constexpr, block_n: tl.constexpr, stage: tl.constexpr,
-                             offs_m: tl.constexpr, offs_n: tl.constexpr, n_ctx: tl.constexpr, non_mask: tl.constexpr, wrap_specialize: tl.constexpr):
+                             offs_m: tl.constexpr, offs_n: tl.constexpr, n_ctx: tl.constexpr, non_mask: tl.constexpr, warp_specialize: tl.constexpr):
     # print(f"Stage : {stage}")
     if stage == 1:
         lo, hi = 0, start_m*block_m
@@ -22,7 +22,7 @@ def _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v,
 
     offsetk_y = offset_y + lo
     offsetv_y = offset_y + lo
-    for start_n in tl.range(lo, hi, block_n, wrap_specialize=wrap_specialize):
+    for start_n in tl.range(lo, hi, block_n, warp_specialize=warp_specialize):
         # print(f"start_n : {start_n} : offset of k [{offsetk_y},0]")
         k = desc_k.load([offsetk_y,0]).T
         qk = tl.dot(q, k)
@@ -40,7 +40,9 @@ def _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v,
         # -- compute correction factor
         alpha = tl.math.exp2(m_i - m_ij)
         l_ij = tl.sum(p, 1)
+
         acc = acc * alpha[:, None]
+
         # print(f"Offset of v [0, {offsetv_y}]")
         v = desc_v.load([offsetv_y, 0])
         p = p.to(dtype)
@@ -49,12 +51,13 @@ def _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v,
         m_i = m_ij
         offsetk_y += block_n
         offsetv_y += block_n
+    return acc, l_i, m_i
 
 @triton.jit
 def _attention_forward(sm_scale, max_tensor, num_heads, n_ctx, desc_q, desc_k, desc_v, desc_o,
                        hidden_dim: tl.constexpr, block_m: tl.constexpr, block_n: tl.constexpr, mask_region: tl.constexpr,
-                       wrap_specialize: tl.constexpr):
-    dtype = torch.bfloat16
+                       warp_specialize: tl.constexpr):
+    dtype = tl.bfloat16
     assert block_n <= hidden_dim
     start_m = tl.program_id(0)
     off_h = tl.program_id(1)
@@ -67,59 +70,59 @@ def _attention_forward(sm_scale, max_tensor, num_heads, n_ctx, desc_q, desc_k, d
     desc_k = tl.make_tensor_descriptor(desc_k, shape=[y_dim, hidden_dim], strides=[hidden_dim, 1],
                                      block_shape=[block_n, hidden_dim])
     desc_o = tl.make_tensor_descriptor(desc_o, shape=[y_dim, hidden_dim], strides=[hidden_dim, 1],
-                                     block_shape=[block_n, hidden_dim])
+                                     block_shape=[block_m, hidden_dim])
     offset_y = off_h*n_ctx
     # print(f"offset_y : {offset_y}")
     qo_offset_y = offset_y + start_m*block_m
     # print(f"qo_offset_y : {qo_offset_y}")
-    offs_m = start_m*block_m + torch.arange(0, block_m)
-    offs_n = torch.arange(0, block_n)
+    offs_m = start_m*block_m + tl.arange(0, block_m)
+    offs_n = tl.arange(0, block_n)
     # print(f"offs_m : {offs_m}")
     # print(f"offs_n : {offs_n}")
     # initialize pointer to m and l
-    m_i = torch.zeros([block_m], dtype=torch.float32) - float("inf")
-    l_i = torch.zeros([block_m], dtype=torch.float32) + 1.0
-    acc = torch.zeros([block_m, hidden_dim], dtype=torch.float32)
+    m_i = tl.zeros([block_m], dtype=tl.float32) - float("inf")
+    l_i = tl.zeros([block_m], dtype=tl.float32) + 1.0
+    acc = tl.zeros([block_m, hidden_dim], dtype=tl.float32)
     # load scales
     qk_scale = sm_scale
     qk_scale *= 1.44269504 #1/log(2)
     q = desc_q.load([qo_offset_y,0])
     # print(f"q load : {[qo_offset_y, 0]}")
-    # q = torch.randn([qo_offset_y, 0], dtype=dtype, device=device, requires_grad=True)
     if mask_region:
-        _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v, offset_y, dtype, start_m, qk_scale, block_m, hidden_dim, block_n, 1, offs_m, offs_n, n_ctx, False, wrap_specialize)
-        _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v, offset_y, dtype, start_m, qk_scale, block_m, hidden_dim, block_n, 2, offs_m, offs_n, n_ctx, False, wrap_specialize)
+        acc, l_i, m_i = _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v, offset_y, dtype, start_m, qk_scale, block_m, hidden_dim, block_n, 1, offs_m, offs_n, n_ctx, False, warp_specialize)
+        acc, l_i, m_i = _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v, offset_y, dtype, start_m, qk_scale, block_m, hidden_dim, block_n, 2, offs_m, offs_n, n_ctx, False, warp_specialize)
     else:
-        _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v, offset_y, dtype, start_m, qk_scale, block_m, hidden_dim, block_n, 2, offs_m, offs_n, n_ctx, True, wrap_specialize)
+        acc, l_i, m_i = _attention_forward_inner_mask(acc, l_i, m_i, q, desc_k, desc_v, offset_y, dtype, start_m, qk_scale, block_m, hidden_dim, block_n, 2, offs_m, offs_n, n_ctx, True, warp_specialize)
     m_i += tl.math.log2(l_i)
     acc = acc / l_i[:, None]
     m_ptrs = max_tensor + off_h * n_ctx + offs_m
     tl.store(m_ptrs, m_i)
     desc_o.store([qo_offset_y, 0], acc.to(dtype))     
 
-# Assumption that it is used only for causal case
-def attention(q, k, v, block_m, block_n, num_heads, sm_scale, device, q_index, kv_index, wrap_specialize=True):
-    HEAD_DIM_Q, HEAD_DIM_K, HEAD_DIM_V = q.shape[-1], k.shape[-1], v.shape[-1]
-    q_with_head = q.unsqueeze(0).expand(num_heads, -1, -1)
-    k_with_head = k.unsqueeze(0).expand(num_heads, -1, -1)
-    v_with_head = v.unsqueeze(0).expand(num_heads, -1, -1)
-    o_with_head = torch.empty_like(q_with_head)
-    # print(q_with_head.shape, k_with_head.shape, v_with_head.shape)
-    BLOCK_M = block_m
-    BLOCK_N = block_n
-    M = torch.empty(q_with_head.shape[0], q_with_head.shape[1], device=device, dtype=torch.float32)
-    # print(f"Max tokens : {M.shape}")
-    grid = (q_with_head.shape[1]//BLOCK_M, num_heads, 1)
-    # print(f"Grid : {grid}")
-    # Attention forward
-    if q_index == kv_index:
-        # mask region
-        _attention_forward[grid](sm_scale, M, num_heads, q_with_head.shape[1],
-                        q_with_head, k_with_head, v_with_head, o_with_head,
-                        q_with_head.shape[-1], BLOCK_M, BLOCK_N, True, wrap_specialize)
-    else:
-        # non-mask region
-        _attention_forward[grid](sm_scale, M, num_heads, q_with_head.shape[1],
-                        q_with_head, k_with_head, v_with_head, o_with_head,
-                        q_with_head.shape[-1], BLOCK_M, BLOCK_N, False, wrap_specialize)
-    return o_with_head
+class _attention:
+  # Assumption that it is used only for causal case
+  @staticmethod
+  def forward(q, k, v, block_m, block_n, num_heads, n_ctx, hidden_dim, sm_scale, device, q_index, kv_index, warp_specialize=True):
+      # HEAD_DIM_Q, HEAD_DIM_K, HEAD_DIM_V = q.shape[-1], k.shape[-1], v.shape[-1]
+      
+      # q_with_head = q.unsqueeze(0).expand(num_heads, -1, -1)
+      # k_with_head = k.unsqueeze(0).expand(num_heads, -1, -1)
+      # v_with_head = v.unsqueeze(0).expand(num_heads, -1, -1)
+      o = torch.empty_like(q)
+      # print(q_with_head.shape, k_with_head.shape, v_with_head.shape)
+      M = torch.empty((q.shape[0], q.shape[1]), device=q.device, dtype=torch.float32)
+      # print(f"Max tokens : {M.shape}")
+      grid = (n_ctx//block_m, num_heads, 1)
+      # print(f"Grid : {grid}")
+      # Attention forward
+      if q_index == kv_index:
+          # mask region
+          _attention_forward[grid](sm_scale, M, num_heads, n_ctx,
+                          q, k, v, o,
+                          hidden_dim, block_m, block_n, True, warp_specialize)
+      else:
+          # non-mask region
+          _attention_forward[grid](sm_scale, M, num_heads, q.shape[1],
+                          q, k, v, o,
+                          hidden_dim, block_m, block_n, False, warp_specialize)
+      return o
--- tests/transformer/FusedAttentionTest.py
+++ tests/transformer/FusedAttentionTest.py
@@ -1,32 +1,38 @@
 import torch
 from torch import nn
+import triton
 
-from transformer.FusedAttention import attention
+from transformer.FusedAttention import _attention
+
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
 
 def test_attention(device, q_index, kv_index):
-    block_m, block_n, tokens, total_vocab, world_size, num_heads, sm_scale, embedding = configs()
+    block_m, block_n, tokens, hiddens, total_vocab, world_size, num_heads, sm_scale, embedding = configs()
     tokens_per_gpu = tokens // world_size
-    sample_input = torch.randint(low=0, high=total_vocab, size=(tokens_per_gpu,)).tolist()
-    input = torch.tensor(sample_input, dtype=torch.int32)
-    input_q = embedding(input)
-    input_k = embedding(input)
-    input_v = embedding(input)
-    output_o = attention(input_q, input_k, input_v, block_m, block_n, num_heads, sm_scale, device, q_index, kv_index)
+    dtype=torch.bfloat16
+    q = torch.randn((num_heads, tokens_per_gpu, hiddens), dtype=dtype, device=DEVICE, requires_grad=True)
+    k = torch.randn((num_heads, tokens_per_gpu, hiddens), dtype=dtype, device=DEVICE, requires_grad=True)
+    v = torch.randn((num_heads, tokens_per_gpu, hiddens), dtype=dtype, device=DEVICE, requires_grad=True)
+        
+    attention = _attention
+    output_o = attention.forward(q, k, v, block_m, block_n, num_heads, tokens_per_gpu, hiddens, sm_scale, DEVICE, q_index, kv_index)
     print(output_o.shape)
 
 def configs():
     num_heads = 8
     world_size = 4
-    tokens = 12*world_size
-    hiddens = 8
+    tokens = 256*world_size
+    hiddens = 1024
     total_vocab = 200021
     dropout = 0.1
-    sm_scale = 0.5
-    block_m = 4
-    block_n = 2
+    sm_scale = 1.3
+    block_m = 64
+    block_n = 32
     embedding = nn.Embedding(total_vocab, hiddens)
-    return block_m, block_n, tokens, total_vocab, world_size, num_heads, sm_scale, embedding
+    return block_m, block_n, tokens, hiddens, total_vocab, world_size, num_heads, sm_scale, embedding
 
 if __name__ == "__main__":
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
   test_attention(device, 1, 0)
+  # only works on post-Ampere GPUs right now
+  # bench_flash_attention.run(save_path=".", print_data=True)
