770741a76a63b89590189b641f4d58d46b74b951
debashis-das <2009debashis@gmail.com>
2026-03-28 21:18:09 +0530

    Deb || revert dtype in backward

--- src/kernels/AttentionBackwardKernelBatch.py
+++ src/kernels/AttentionBackwardKernelBatch.py
@@ -61,9 +61,9 @@ def _attention_bwd(q, k, v, do, dq, dk, dv, m, d,
   desc_dq = tl.make_tensor_descriptor(dq, shape=[y_dim, hidden_dim], strides=[hidden_dim, 1],
                                         block_shape=[block_m, hidden_dim])
   
-  dvalue = tl.zeros([block_m, hidden_dim], dtype=tl.float16)
-  dkey = tl.zeros([block_m, hidden_dim], dtype=tl.float16)
-  dquery = tl.zeros([block_m, hidden_dim], dtype=tl.float16)
+  dvalue = tl.zeros([block_m, hidden_dim], dtype=tl.float32)
+  dkey = tl.zeros([block_m, hidden_dim], dtype=tl.float32)
+  dquery = tl.zeros([block_m, hidden_dim], dtype=tl.float32)
 
   mask_block_n:tl.constexpr = block_n // bulk_slice_factor
   key = desc_k.load([init_offset,0])
