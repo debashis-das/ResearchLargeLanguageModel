3c0e61eb29c6a6ff94c5401ed943efc8054c9ce2
debashis-das <2009debashis@gmail.com>
2026-03-28 21:27:46 +0530

    Deb || removed print logs

--- src/app/distkernelbatch.py
+++ src/app/distkernelbatch.py
@@ -47,7 +47,7 @@ class MultiGPUExecutor(nn.Module):
       # src_tokens = torch.tensor(tokens, dtype=torch.int32, device=DEVICE)
       X = self.embedding(src_tokens)
       for _ in range(24):
-        print(f"X shape : {X.shape}")
+        # print(f"X shape : {X.shape}")
         X = self.rms1(X)
         q, k, v = self.W_q(X), self.W_k(X), self.W_v(X)
         q, k = self.rope_embedding(q, k) 
@@ -59,15 +59,15 @@ class MultiGPUExecutor(nn.Module):
         block_m = 32
         block_n = 16
         grid_fwd = (n_ctx//block_m, Config.num_heads*Config.batch, 1)
-        print(f"Grid (fwd) : {grid_fwd} : q{q.shape} strides : {q.stride()} : k{k.shape} strides : {k.stride()} : v{v.shape} strides : {v.stride()}")
+        # print(f"Grid (fwd) : {grid_fwd} : q{q.shape} strides : {q.stride()} : k{k.shape} strides : {k.stride()} : v{v.shape} strides : {v.stride()}")
         output = self.attention(q, k, v, Config.batch, Config.num_heads, n_ctx, Config.hiddens, 
                                 Config.sm_scale, world_size, self.rank)
-        print(f"Output ({rank},{rank}): {output.shape}")
+        # print(f"Output ({rank},{rank}): {output.shape}")
         output = output.permute(0, 2, 1, 3).reshape(Config.batch, self.tokens_per_gpu,-1)
         v = v.permute(0, 2, 1, 3).reshape(Config.batch, self.tokens_per_gpu, -1)
         # print(f"Output after permute & reshape ({rank},{rank}) o:{output.shape}, v:{v.shape}")
         x_residual = output + v
-        print(f"x_residual : {output.shape}, {v.shape}, {x_residual.shape}")
+        # print(f"x_residual : {output.shape}, {v.shape}, {x_residual.shape}")
         y_rms = self.rms2(x_residual)
         z = self.mlp(y_rms)
         X = x_residual + self.W_down(z)
