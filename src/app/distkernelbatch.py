92bf00d9e70260b654b09d8077f3dfad34ccaefb
debashis-das <2009debashis@gmail.com>
2026-03-28 21:59:50 +0530

    Deb || printing the whole loss

--- src/app/distkernelbatch.py
+++ src/app/distkernelbatch.py
@@ -81,8 +81,9 @@ class MultiGPUExecutor(nn.Module):
       src_tokens = src_tokens.view(B*T)
       shift_labels = src_tokens[...,1:].contiguous()
       shift_logits = logits[...,:-1,:].contiguous()
-      # print(f"shift_logits: {shift_logits.shape}, shift_labels: {shift_labels.shape}")
+      print(f"shift_logits: {shift_logits.shape}, shift_labels: {shift_labels.shape}")
       loss = self.loss_fn(shift_logits, shift_labels.long())
+      print(f"Loss : {loss} ")
       if all_logits:
         return all_logits, loss
       return output_logits, loss
