a325b94d78531e0e35d32fdf3633c0fe5b24c994
debashis-das <2009debashis@gmail.com>
2026-03-31 22:26:02 +0530

    Deb || shapes of q and k

--- src/transformer/TransformerLayer.py
+++ src/transformer/TransformerLayer.py
@@ -31,6 +31,7 @@ class TransformerLayer(nn.Module):
         X = self.rms1(X)
         q, k, v = self.W_q(X), self.W_k(X), self.W_v(X)
         q, k = self.rope_embedding(q, k) 
+        print(f"q.shape: {q.shape}, k.shape: {k.shape}")
         self.exit_on_nan(q, f"NaN in query after rope embedding in rank {self.rank}")
         self.exit_on_nan(k, f"NaN in key after rope embedding in rank {self.rank}")
         q = q.reshape(Config.batch, self.tokens_per_gpu, Config.num_heads, -1).permute(0, 2, 1, 3).contiguous()
