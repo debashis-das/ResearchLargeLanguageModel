Deb || gradients printing fixed

src/transformer/FusedAttentionBatch.py
@@ -407,9 +407,9 @@ class _attention(torch.autograd.Function):
           num_stages=NUM_STAGES,  #
           CAUSAL=True,  #
       )
-      logging.info(f'dq : Minumum gradient : {dq_doc.grad.dtype} : {dq_doc.grad.min()}, Maximum gradient : {dq_doc.grad.max()}')
-      logging.info(f'dk : Minumum gradient : {dk_doc.grad.dtype} :{dk_doc.grad.min()}, Maximum gradient : {dk_doc.grad.max()}')
-      logging.info(f'dv : Minumum gradient : {dv_doc.grad.dtype} :{dv_doc.grad.min()}, Maximum gradient : {dv_doc.grad.max()}')
+      logging.info(f'dq : Minumum gradient : {dq_doc.dtype} : {dq_doc.min()}, Maximum gradient : {dq_doc.max()}')
+      logging.info(f'dk : Minumum gradient : {dk_doc.dtype} :{dk_doc.min()}, Maximum gradient : {dk_doc.max()}')
+      logging.info(f'dv : Minumum gradient : {dv_doc.dtype} :{dv_doc.min()}, Maximum gradient : {dv_doc.max()}')
       logging.info("-----------------------------")
       dq = torch.empty_like(q)
       dk = torch.empty_like(k)
@@ -422,9 +422,9 @@ class _attention(torch.autograd.Function):
       )
       _attention_bwd[grid_bwd](q, k, v, do, dq, dk, dv, M, delta, sm_scale, BATCH, N_HEAD, N_CTX,
                                HEAD_DIM, BLOCK_M, BLOCK_N, num_warps=NUM_WARPS, num_stages=NUM_STAGES, CAUSAL=CAUSAL)
-      logging.info(f'dq : Minumum gradient : {dq.grad.dtype} : {dq.grad.min()}, Maximum gradient : {dq.grad.max()}')
-      logging.info(f'dk : Minumum gradient : {dk.grad.dtype} :{dk.grad.min()}, Maximum gradient : {dk.grad.max()}')
-      logging.info(f'dv : Minumum gradient : {dv.grad.dtype} :{dv.grad.min()}, Maximum gradient : {dv.grad.max()}')
+      logging.info(f'dq : Minumum gradient : {dq.dtype} : {dq.min()}, Maximum gradient : {dq.max()}')
+      logging.info(f'dk : Minumum gradient : {dk.dtype} :{dk.min()}, Maximum gradient : {dk.max()}')
+      logging.info(f'dv : Minumum gradient : {dv.dtype} :{dv.min()}, Maximum gradient : {dv.max()}')
       logging.info("-----------------------------")
       sys.exit()
       # if world_size != 1: