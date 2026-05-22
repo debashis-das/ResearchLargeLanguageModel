9d94a3314e4af1ee5c5c230c27cb0cc5d7f6bd54
debashis-das <2009debashis@gmail.com>
2026-05-22 19:07:34 +0530

    Deb || try catch for RMS Norm

--- src/transformer/RMSNorm.py
+++ src/transformer/RMSNorm.py
@@ -10,9 +10,12 @@ class RMSNorm(nn.Module):
     
     # @torch.compile
     def forward(self, hidden_state: torch.Tensor):
-        input_dtype = hidden_state.dtype
-        hidden_state = hidden_state.to(torch.float32)
-        print(f"[{self.rms_info}] [Shape {hidden_state.shape}]")
-        variance = hidden_state.pow(2).mean(-1, keepdim=True)
-        hidden_state = hidden_state*torch.rsqrt(variance+self.variance_epsilon)
-        return self.weight*hidden_state.to(input_dtype)
+        try:
+            input_dtype = hidden_state.dtype
+            hidden_state = hidden_state.to(torch.float32)
+            variance = hidden_state.pow(2).mean(-1, keepdim=True)
+            hidden_state = hidden_state*torch.rsqrt(variance+self.variance_epsilon)
+            return self.weight*hidden_state.to(input_dtype)
+        except Exception as e:
+            print(f"[{self.rms_info}] [Shape {hidden_state.shape}] : Exception in RMSNorm forward: {e}")
+            raise e
