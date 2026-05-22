c53def682588223137f2509419ff25b364b3ca93
debashis-das <2009debashis@gmail.com>
2026-05-22 22:15:10 +0530

    Deb || fixed offsets for dq, dk and dv

--- src/kernels/AttentionBackwardKernelBatch.py
+++ src/kernels/AttentionBackwardKernelBatch.py
@@ -34,15 +34,22 @@ def _attention_bwd_dkdv(dkey, dvalue, m, d, q, k, v, do,
     mask_offset_along_n = ctxid*block_m + base_mask + tl.arange(0, block_n)
     if causal and mask:
       num_steps = block_m // block_n
+      offset_block_n = init_offset + offset_along_n[:, None] + offset_along_h[None, :]
+      offset_block_n_T = init_offset + offset_along_n[None, :] + offset_along_h[:, None]
     elif causal and not mask:
       num_steps = (n_ctx - (ctxid+1)*block_m) // block_n
       mask_offset_along_n = (ctxid+1)*block_m + base_mask + tl.arange(0, block_n)
+      offset_block_n = block_m*hidden_dim + init_offset + offset_along_n[:, None] + offset_along_h[None, :]
+      offset_block_n_T = block_m*hidden_dim + init_offset + offset_along_n[None, :] + offset_along_h[:, None]  
     else:
       num_steps = n_ctx // block_n
-    
-    offset_block_n = init_offset + offset_along_n[:, None] + offset_along_h[None, :]
-    offset_block_n_T = init_offset + offset_along_n[None, :] + offset_along_h[:, None]
     for _ in range(num_steps):
+      if causal and mask:
+        assert init_offset <= offset_block_n_T < init_offset + block_m*hidden_dim + base_mask*hidden_dim, f"[dkdv][Mask : {mask}]offset_block_n_T: {offset_block_n_T}, ctxid: {ctxid}"
+        assert init_offset <= offset_block_n < init_offset + block_m*hidden_dim + base_mask*hidden_dim, f"[dkdv][Mask : {mask}]offset_block_n: {offset_block_n}, ctxid: {ctxid}"
+      elif causal and not mask:
+        assert init_offset + block_m*hidden_dim < offset_block_n_T <= n_ctx*hidden_dim + base_mask*hidden_dim, f"[dkdv][Mask : {mask}]offset_block_n_T: {offset_block_n_T}, ctxid: {ctxid}"
+        assert init_offset + block_m*hidden_dim < offset_block_n <= n_ctx*hidden_dim + base_mask*hidden_dim, f"[dkdv][Mask : {mask}]offset_block_n: {offset_block_n}, ctxid: {ctxid}"
       queryT = tl.load(q + offset_block_n_T)  # pre-load to L1
       d_of_o = tl.load(do + offset_block_n)  # pre-load to L1
       max_tensor = tl.load(m + mask_offset_along_n)  # pre-load to L1
@@ -82,9 +89,9 @@ def _attention_bwd_dq(dquery, m, d, q, k, v, do,
     
     for _ in range(num_steps):
       if causal and mask:
-        assert (ctxid*block_m*hidden_dim + offset_batch_head) <= offset_block_n_T < ((ctxid+1)*block_m*hidden_dim + offset_batch_head), f"[Mask : {mask}]offset_block_n_T: {offset_block_n_T}, ctxid: {ctxid}, block_m: {block_m}, hidden_dim: {hidden_dim}, offset_batch_head: {offset_batch_head}"
+        assert (ctxid*block_m*hidden_dim + offset_batch_head) <= offset_block_n_T < ((ctxid+1)*block_m*hidden_dim + offset_batch_head), f"[dq][Mask : {mask}]offset_block_n_T: {offset_block_n_T}, ctxid: {ctxid}, block_m: {block_m}"
       elif causal and not mask:
-        assert offset_batch_head < offset_block_n_T <= (ctxid*block_m*hidden_dim + offset_batch_head), f"[Mask : {mask}]offset_block_n_T: {offset_block_n_T}, ctxid: {ctxid}, block_m: {block_m}, hidden_dim: {hidden_dim}, offset_batch_head: {offset_batch_head}"
+        assert offset_batch_head < offset_block_n_T <= (ctxid*block_m*hidden_dim + offset_batch_head), f"[dq][Mask : {mask}]offset_block_n_T: {offset_block_n_T}, ctxid: {ctxid}, block_m: {block_m}"
       keyT = tl.load(k + offset_block_n_T)  # pre-load to L1
       valueT = tl.load(v + offset_block_n_T)  # pre-load to L1
       qkT = tl.dot(q, keyT)
--- src/transformer/FusedAttentionBatch.py
+++ src/transformer/FusedAttentionBatch.py
@@ -405,26 +405,26 @@ class _attention(torch.autograd.Function):
           num_stages=NUM_STAGES,  #
           CAUSAL=True,  #
       )
-      # logging.info(f'dq : Minumum gradient : {dq_doc.dtype} : {dq_doc.min()}, Maximum gradient : {dq_doc.max()}')
-      # logging.info(f'dk : Minumum gradient : {dk_doc.dtype} :{dk_doc.min()}, Maximum gradient : {dk_doc.max()}')
-      # logging.info(f'dv : Minumum gradient : {dv_doc.dtype} :{dv_doc.min()}, Maximum gradient : {dv_doc.max()}')
-      # logging.info("-----------------------------")
-      # dq = torch.empty_like(q)
-      # dk = torch.empty_like(k)
-      # dv = torch.empty_like(v)
-      # BLOCK_M, BLOCK_N = 128, 32
-      # grid_bwd = lambda META: (
-      #     triton.cdiv(N_CTX, BLOCK_M),
-      #     N_HEAD * BATCH,
-      #     1
-      # )
-      # _attention_bwd[grid_bwd](q, arg_k, v, do, dq, dk, dv, M, delta, sm_scale, BATCH, N_HEAD, N_CTX,
-      #                          HEAD_DIM, BLOCK_M, BLOCK_N, num_warps=NUM_WARPS, num_stages=NUM_STAGES, CAUSAL=CAUSAL)
-      # logging.info(f'dq : Minumum gradient : {dq.dtype} : {dq.min()}, Maximum gradient : {dq.max()}')
-      # logging.info(f'dk : Minumum gradient : {dk.dtype} :{dk.min()}, Maximum gradient : {dk.max()}')
-      # logging.info(f'dv : Minumum gradient : {dv.dtype} :{dv.min()}, Maximum gradient : {dv.max()}')
-      # logging.info("-----------------------------")
-      # sys.exit()
+      logging.info(f'dq : Minumum gradient : {dq_doc.dtype} : {dq_doc.min()}, Maximum gradient : {dq_doc.max()}')
+      logging.info(f'dk : Minumum gradient : {dk_doc.dtype} :{dk_doc.min()}, Maximum gradient : {dk_doc.max()}')
+      logging.info(f'dv : Minumum gradient : {dv_doc.dtype} :{dv_doc.min()}, Maximum gradient : {dv_doc.max()}')
+      logging.info("-----------------------------")
+      dq = torch.empty_like(q)
+      dk = torch.empty_like(k)
+      dv = torch.empty_like(v)
+      BLOCK_M, BLOCK_N = 128, 32
+      grid_bwd = lambda META: (
+          triton.cdiv(N_CTX, BLOCK_M),
+          N_HEAD * BATCH,
+          1
+      )
+      _attention_bwd[grid_bwd](q, arg_k, v, do, dq, dk, dv, M, delta, sm_scale, BATCH, N_HEAD, N_CTX,
+                               HEAD_DIM, BLOCK_M, BLOCK_N, num_warps=NUM_WARPS, num_stages=NUM_STAGES, CAUSAL=CAUSAL)
+      logging.info(f'dq : Minumum gradient : {dq.dtype} : {dq.min()}, Maximum gradient : {dq.max()}')
+      logging.info(f'dk : Minumum gradient : {dk.dtype} :{dk.min()}, Maximum gradient : {dk.max()}')
+      logging.info(f'dv : Minumum gradient : {dv.dtype} :{dv.min()}, Maximum gradient : {dv.max()}')
+      logging.info("-----------------------------")
+      sys.exit()
       # if world_size != 1:
       #       exe_order_per_rank_v, exe_order_per_rank_h, exe_order_per_rank_unaligned  = identify_nodes_for_qkv(world_size)
       #       dist.barrier()
@@ -499,6 +499,6 @@ class _attention(torch.autograd.Function):
       # logging.debug(f"[Attention] dv: {torch.isnan(dv).any()} : dv.max(): {dv.abs().max()} : dv.min(): {dv.abs().min()}")
       # logging.debug(f"[Attention] dk: {torch.isnan(dk).any()} : dk.max(): {dk.abs().max()} : dk.min(): {dk.abs().min()}")
       # logging.debug(f"[Attention] dq: {torch.isnan(dq).any()} : dq.max(): {dq.abs().max()} : dq.min(): {dq.abs().min()}")
-      # return dq, dk, dv, None, None, None, None, None, None, None, None, None
-      return dq_doc, dk_doc, dv_doc, None, None, None, None, None, None, None, None, None
+      return dq, dk, dv, None, None, None, None, None, None, None, None, None
+      # return dq_doc, dk_doc, dv_doc, None, None, None, None, None, None, None, None, None
   
