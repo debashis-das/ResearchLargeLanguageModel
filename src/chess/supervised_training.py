d4fe5429bf835de931c49d392ff26e415c01ae59
debashis-das <2009debashis@gmail.com>
2026-06-06 22:18:09 +0530

    Deb || decreased batch size to 4

--- src/chess/supervised_training.py
+++ src/chess/supervised_training.py
@@ -20,7 +20,7 @@ device = model.device
 
 def sft_train():
     try:
-        batch_size = 8
+        batch_size = 4
         optimizer = torch.optim.AdamW(model_with_lora.parameters(), lr=1e-4)
         for i in range(2):
             current_paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/{i:06d}-sl.parquet"
