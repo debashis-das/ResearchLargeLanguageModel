9cc2d78a750a843e927c8fe32b53ff941ec458ed
debashis-das <2009debashis@gmail.com>
2026-06-08 23:43:34 +0530

    Deb || model and paraquet path changed

--- src/chess/supervised_training.py
+++ src/chess/supervised_training.py
@@ -7,8 +7,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from lora.LoRAFineTuning import LoRAFineTuning
 
-# model_path = "/home/model"
-model_path = "C:\\Users\\DebashisDas\\personal\\models\\Qwen"
+model_path = "/home/model"
+# model_path = "C:\\Users\\DebashisDas\\personal\\models\\Qwen"
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 dtype = torch.bfloat16
 model = AutoModelForCausalLM.from_pretrained(
@@ -26,8 +26,8 @@ def sft_train():
         running_loss = torch.zeros([1], dtype=torch.float32, device=device)
         optimizer = torch.optim.AdamW(model_with_lora.parameters(), lr=1e-5)
         for i in range(2):
-            # current_paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/{i:06d}-sl.parquet"
-            current_paraquet = f"src\\chess\\paraquets\\{i:06d}-sl.parquet"
+            current_paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/{i:06d}-sl.parquet"
+            # current_paraquet = f"src\\chess\\paraquets\\{i:06d}-sl.parquet"
             df_input = pd.read_parquet(current_paraquet)
             loss = None
             for _, row in df_input.iterrows():
