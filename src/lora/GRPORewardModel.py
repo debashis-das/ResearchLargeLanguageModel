e0b3ea77faeec887acaf36a18bfa3fec2712a918
debashis-das <2009debashis@gmail.com>
2026-06-09 22:49:43 +0530

    Deb || changed few parameters in GRPO

--- src/lora/GRPORewardModel.py
+++ src/lora/GRPORewardModel.py
@@ -10,7 +10,7 @@ from lora.LoRAFineTuning import LoRAFineTuning
 
 class GRPORewardModel(nn.Module):
 
-    def __init__(self, tokenizer: AutoTokenizer, model: LoRAFineTuning, grpo_batch: int, device="cpu", dtype=torch.float16, total_generation_length=400, generation_evalution_length=300):
+    def __init__(self, tokenizer: AutoTokenizer, model: LoRAFineTuning, grpo_batch: int, device="cpu", dtype=torch.float16, total_generation_length=400):
         super(GRPORewardModel, self).__init__()
         self.tokenizer = tokenizer
         self.grpo_batch = grpo_batch
@@ -18,7 +18,8 @@ class GRPORewardModel(nn.Module):
         self.device = device
         self.dtype = dtype
         self.total_generation_length = total_generation_length
-        self.generation_evalution_length = generation_evalution_length
+        self.reward_moves = 5
+        self.value_fn_moves = 10
         self.softmax = nn.Softmax(dim=-1)
         self.gamma = 0.99
 
@@ -67,11 +68,11 @@ class GRPORewardModel(nn.Module):
         game = ChessGame()
         # input extraction and create board state based on the input moves
         input_moves =  (output_with_prompt.rsplit("Generation Instructions:")[0].strip().rsplit("moves:")[-1].strip())
-        print(f"Input moves extracted for board state initialization: {input_moves}")
+        # print(f"Input moves extracted for board state initialization: {input_moves}")
         game, _, _, move_no = self.board_state(input_moves, game)
 
         generation_moves = (output_with_prompt.rsplit("moves:")[-1].strip())
-        print(f"Generation moves extracted for reward calculation: {generation_moves}")
+        # print(f"Generation moves extracted for reward calculation: {generation_moves}")
         play_as = (output_with_prompt.rsplit("play_as:")[-1].strip().split("moves:")[0].strip())
         reward = 0.0
         try:
@@ -88,14 +89,15 @@ class GRPORewardModel(nn.Module):
         except Exception as e:
             print(f"An error occurred during move processing: {e}")
             reward -= 1.0
-        print(game.board_string())
+        if generation_move_no - move_no > 0:
+            print(game.board_string())
         print(f"Generated new moves : {generation_move_no - move_no} : Reward : {reward}")
         return reward
 
 
     def extract_reward(self, tensor_per_generation: torch.Tensor):
         value_fn_str = self.tokenizer.decode(tensor_per_generation, skip_special_tokens=True)
-        print(f"Value function string :  {value_fn_str}")
+        # print(f"Value function string :  {value_fn_str}")
         reward = self.chess_reward_function(value_fn_str)
         return reward
     
@@ -107,31 +109,30 @@ class GRPORewardModel(nn.Module):
         # print(f"Input shape : {X.shape}")
         output_tensor = self.model.generate(X, max_new_tokens=self.total_generation_length)
         # print(f"Output tensor shape: {output_tensor.shape}")
-        reward_consideration_reverse_idx = (self.total_generation_length - self.generation_evalution_length)*-1
         advantage_batch = []
         nll_batch = [] 
         for idx, tensor_per_generation in enumerate(output_tensor):
             considered_tensor = tensor_per_generation
             # self.debug_logs(X, idx, tensor_per_generation)
             reward = self.extract_reward(considered_tensor)
-
         #     value_t_with_k_reward = self.extract_reward(tensor_per_generation, moves_to_consider=20)
         #     # print(f"Reward extracted: {reward} : Value function with k reward extracted: {value_t_with_k_reward}")
         #     value_t_reward = 0.0
-        #     mask_addition = considered_tensor.shape[-1] - attention_mask.shape[-1]
-        #     extra_mask = torch.ones(mask_addition, dtype=attention_mask.dtype, device=attention_mask.device).unsqueeze(0)
-        #     training_mask = torch.cat([attention_mask, extra_mask], dim=-1)
+            mask_addition = considered_tensor.shape[-1] - attention_mask.shape[-1]
+            extra_mask = torch.ones(mask_addition, dtype=attention_mask.dtype, device=attention_mask.device).unsqueeze(0)
+            training_mask = torch.cat([attention_mask, extra_mask], dim=-1)
         #     # print(f"Attention mask shape after concatenation : {attention_mask.shape} : {extra_mask.shape} : {training_mask.shape}")
-        #     _, nll = self.model(considered_tensor.unsqueeze(0), attention_mask=training_mask)
-        #     advantage = reward +(value_t_with_k_reward - value_t_reward)
-        #     nll_batch.append(nll)
-        #     advantage_batch.append(torch.tensor(advantage, dtype=self.dtype, device=self.device))
-        # nll_batch = torch.stack(nll_batch)
-        # advantage_batch = torch.tanh(torch.stack(advantage_batch))
-        # print(f"Advantage : {[a.item() for a in advantage_batch]} : Loss : {[loss.item() for loss in nll_batch]}")
-        # loss_batch = nll_batch * self.gamma * advantage_batch
-        # print(f"Loss batch : {[loss.item() for loss in loss_batch]}")
-        # return loss_batch.mean()
+            _, nll = self.model(considered_tensor.unsqueeze(0), attention_mask=training_mask)
+            # advantage = reward +(value_t_with_k_reward - value_t_reward)
+            advantage = reward
+            nll_batch.append(nll)
+            advantage_batch.append(torch.tensor(advantage, dtype=self.dtype, device=self.device))
+        nll_batch = torch.stack(nll_batch)
+        advantage_batch = torch.tanh(torch.stack(advantage_batch))
+        print(f"Advantage : {[a.item() for a in advantage_batch]} : Loss : {[loss.item() for loss in nll_batch]}")
+        loss_batch = nll_batch * self.gamma * advantage_batch
+        print(f"Loss batch : {[loss.item() for loss in loss_batch]}")
+        return loss_batch.mean()
 
     def debug_logs(self, X, idx, tensor_per_generation):
         print(f"Input      : {self.tokenizer.decode(X[idx], skip_special_tokens=True)}")
