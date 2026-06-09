08c2b0ffc294c9d2e631c9044ee2f168755a43a1
debashis-das <2009debashis@gmail.com>
2026-06-09 21:08:51 +0530

    Deb || without supervised training

--- src/chess/rl_training.py
+++ src/chess/rl_training.py
@@ -75,4 +75,5 @@ def rl_train(load_path = ""):
         print(f"An error occurred during Parquet generation test: {e}")
 
 if __name__ == "__main__":
-    rl_train(load_path = f"model/qwen-0.6b-with-loRA-sft-model-params")
+    # rl_train(load_path = f"model/qwen-0.6b-with-loRA-sft-model-params")
+    rl_train()
