0e839c37de848bcc65e1f0443b6f9531fa42914a
debashis-das <2009debashis@gmail.com>
2026-06-18 21:44:14 +0530

    Deb || GRPO debug

--- src/lora/GRPORewardModel.py
+++ src/lora/GRPORewardModel.py
@@ -119,6 +119,7 @@ class GRPORewardModel(nn.Module):
             game, _, _, move_no = self.board_state(input_moves, game, play_as=play_as)
             same_generations = 0
             moves_generations_with_extra_text = generation.split("moves:")
+            print(f"moves_generations_with_extra_text : {len(moves_generations_with_extra_text)} : {moves_generations_with_extra_text}")
             if len(moves_generations_with_extra_text) == 0:
                 current_reward = 10.0
                 _, current_reward, all_valid, generation_move_no = self.board_state(generation.strip(), game, current_reward, ignore_moves_till = move_no, play_as=play_as)
@@ -180,7 +181,7 @@ class GRPORewardModel(nn.Module):
         input_sequence_length = x.shape[-1]
         X = x.repeat_interleave(repeats=self.grpo_batch, dim=0)  # Repeat the input tensor for the batch size
         attention_mask = attention_mask.repeat_interleave(repeats=self.grpo_batch, dim=0)  # Repeat the attention mask for the batch size
-        output_tensor = self.model.generate(X, max_new_tokens=self.total_generation_length, temperature=1.3)
+        output_tensor = self.model.generate(X, max_new_tokens=self.total_generation_length, temperature=1.0)
         mask_addition = output_tensor.shape[-1] - attention_mask.shape[-1]
         extra_mask = torch.ones(mask_addition, dtype=attention_mask.dtype, device=attention_mask.device).unsqueeze(0)
         extra_mask = extra_mask.repeat_interleave(repeats=self.grpo_batch, dim=0)  # Repeat the extra mask for the batch size
