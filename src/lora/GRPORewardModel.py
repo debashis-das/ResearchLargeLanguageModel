d11ffda90b68091a9e8477367c16f86e05679695
debashis-das <2009debashis@gmail.com>
2026-06-21 13:18:46 +0530

    Deb || GRPO removed santization for base logits as it doesnt change

--- src/lora/GRPORewardModel.py
+++ src/lora/GRPORewardModel.py
@@ -208,7 +208,6 @@ class GRPORewardModel(nn.Module):
         log_probs = selected_logits - logsumexp
         with torch.no_grad():
             base_logits = self.use_base_model(output_tensor.detach(), attention_mask=training_mask)
-            base_logits = self.sanatize_logits(base_logits)
             logsumexp_base = torch.logsumexp(base_logits, dim=-1)
             selected_base_logits = torch.gather(base_logits, dim=-1, index=actions.unsqueeze(-1)).squeeze(-1)
             base_log_probs = selected_base_logits - logsumexp_base
