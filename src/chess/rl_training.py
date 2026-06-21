60311fed90fd1fc12a8acbf9129d408359dcb1e6
debashis-das <2009debashis@gmail.com>
2026-06-21 18:14:59 +0530

    Deb || print timestep

--- src/chess/rl_training.py
+++ src/chess/rl_training.py
@@ -62,7 +62,7 @@ def rl_train(load_path = ""):
                     optimizer.step()
                     training_timestep += 1
                     # if training_timestep % 1 == 0:
-                    #     print(f"Training timestep: {training_timestep}, Loss: {loss.item()}")
+                    print(f"Training timestep: {training_timestep}, Loss: {loss.item()}")
                 except Exception as e:
                     print(f"An error occurred during model training: {e}")
                     traceback.print_exc()
