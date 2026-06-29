f36f75281f1018c7289654b66daaa836e699d5a1
debashis-das <2009debashis@gmail.com>
2026-06-29 22:26:56 +0530

    Deb || loRA parameters debug

--- src/lora/LoRAFineTuning.py
+++ src/lora/LoRAFineTuning.py
@@ -76,7 +76,7 @@ class LoRAFineTuning(nn.Module):
     def load_lora_parameters(self, load_path):
         lora_state_dict = torch.load(load_path, map_location=self.device)
         for name, module in self.model.named_modules():
-            if "loRA" in name:
+            if name in lora_state_dict:
                 print(f"Loading LoRA parameters for {name}")
                 module.loRA_module.load_state_dict(lora_state_dict[name])
         print(f"LoRA parameters loaded from {load_path}")
