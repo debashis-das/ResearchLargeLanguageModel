b135a20201bccca897df1c15c627f857373b8e5e
debashis-das <2009debashis@gmail.com>
2026-06-29 22:08:42 +0530

    Deb || execute with parameters

--- src/chess/execute_training.py
+++ src/chess/execute_training.py
@@ -2,9 +2,29 @@
 from chess.rl_training import rl_execute
 from chess.supervised_training import sft_execute
 
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Named arguments example")
+    # 1. Standard named argument (with an optional short alias '-sft')
+    parser.add_argument('-sft', '--supervised', type=bool, default=True, help="Is the training supervised? (yes/no)")
+    # 2. Optional named argument with a default fallback value
+    parser.add_argument('-rl', '--reinforcement', type=bool, default=True, help="Is the training reinforcement learning? (yes/no)")
+
+    # Parse inputs
+    args = parser.parse_args()
+
+    # Access them using the long name (without the dashes)
+    print(f"Is the training supervised? {args.supervised}")
+    print(f"Is the training reinforcement learning? {args.reinforcement}")
+
+    # Execute the SFT training
+    if args.supervised:
+        sft_execute()
+    # Execute the RL training
+    if args.reinforcement:
+        rl_execute()
 
 if __name__ == "__main__":
-    # Execute the SFT training
-    sft_execute()
-    # Execute the RL training
-    rl_execute()
+    main()
+    
