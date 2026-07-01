3bd552e17290eb0111a5770c8879b00ec38895bb
debashis-das <2009debashis@gmail.com>
2026-07-01 21:10:11 +0530

    Deb || supervised training for 10 timesteps

--- src/chess/supervised_training.py
+++ src/chess/supervised_training.py
@@ -34,7 +34,7 @@ def sft_train():
             df_shuffled = df_input.sample(frac=1, ignore_index=True)
             loss = None
             for _, row in df_shuffled.iterrows():
-                if training_timestep > 50:
+                if training_timestep > 10:
                     end = True
                     break
                 input_ids = torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
