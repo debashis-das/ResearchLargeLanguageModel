b355065c136aa53c94e316cccacf07b6c7cd8285
debashis-das <2009debashis@gmail.com>
2026-07-02 21:36:46 +0530

    Deb || rl init generation removed

--- src/chess/rl_training.py
+++ src/chess/rl_training.py
@@ -15,18 +15,18 @@ from lora.LoRAFineTuning import LoRAFineTuning
 
 def rl_train(tokenizer=None, model=None, model_with_lora=None, device=None, dtype=None, optimizer=None, replay_buffer=None):
     try:
-        paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/000001-rl.parquet"
-        df_input = pd.read_parquet(paraquet)
-        row = df_input.sample(n=1).iloc[0]
-        input_ids = torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
-        attention_mask = torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
-        input_ids = input_ids.repeat_interleave(repeats=5, dim=0)  # Repeat the input tensor for the batch size
-        attention_mask = attention_mask.repeat_interleave(repeats=5, dim=0)  # Repeat the attention mask for the batch size
+        # paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/000001-rl.parquet"
+        # df_input = pd.read_parquet(paraquet)
+        # row = df_input.sample(n=1).iloc[0]
+        # input_ids = torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
+        # attention_mask = torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
+        # input_ids = input_ids.repeat_interleave(repeats=5, dim=0)  # Repeat the input tensor for the batch size
+        # attention_mask = attention_mask.repeat_interleave(repeats=5, dim=0)  # Repeat the attention mask for the batch size
         grpo_reward_model = GRPORewardModel(tokenizer, model_with_lora, grpo_batch=16, model_device=device, dtype=dtype)
         
-        generation_ids = grpo_reward_model.generate(input_ids, attention_mask=attention_mask)
-        print(f"Generated text: {tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
-        exit()
+        # generation_ids = grpo_reward_model.generate(input_ids, attention_mask=attention_mask)
+        # print(f"Generated text: {tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
+        # exit()
         training_timestep = 0
         recover = False
         for i in range(5):
