c6f2196aa06aded7ffff732f7011f478923e6eaf
debashis-das <2009debashis@gmail.com>
2026-07-02 20:02:19 +0530

    Deb || GRPO temperature removed

--- src/lora/GRPORewardModel.py
+++ src/lora/GRPORewardModel.py
@@ -168,7 +168,7 @@ class GRPORewardModel(nn.Module):
     
     def generate(self, input_ids, attention_mask):
         with torch.no_grad():
-            output_tensor = self.model.generate(input_ids.to(self.model_device), attention_mask=attention_mask.to(self.model_device), max_new_tokens=self.total_generation_length, temperature=self.temperature)
+            output_tensor = self.model.generate(input_ids.to(self.model_device), attention_mask=attention_mask.to(self.model_device), max_new_tokens=self.total_generation_length)
         return output_tensor
 
     
