b27f897a0544b7e7ac5a5135eb9abe9895ed9991
debashis-das <2009debashis@gmail.com>
2026-07-02 22:24:22 +0530

    Deb || removed remperature

--- src/lora/GRPORewardModel.py
+++ src/lora/GRPORewardModel.py
@@ -213,8 +213,8 @@ class GRPORewardModel(nn.Module):
             output_tensor = self.model.generate(input_ids.to(self.model_device), 
                                             attention_mask=attention_mask.to(self.model_device), 
                                             max_new_tokens=self.total_generation_length, 
-                                            sampling=True,
-                                            temperature=0.1
+                                            sampling=True
+                                            # temperature=0.1
                                             )
         return output_tensor
 
