bf97630529c54529c25dfa0ea9d61478e51bb4dc
debashis-das <2009debashis@gmail.com>
2026-07-03 19:33:08 +0530

    Deb || GRPO size reduced to 12

--- src/chess/rl_training.py
+++ src/chess/rl_training.py
@@ -22,7 +22,7 @@ def rl_train(tokenizer=None, model=None, model_with_lora=None, device=None, dtyp
         # attention_mask = torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
         # input_ids = input_ids.repeat_interleave(repeats=5, dim=0)  # Repeat the input tensor for the batch size
         # attention_mask = attention_mask.repeat_interleave(repeats=5, dim=0)  # Repeat the attention mask for the batch size
-        grpo_reward_model = GRPORewardModel(tokenizer, model_with_lora, grpo_batch=16, model_device=device, dtype=dtype)
+        grpo_reward_model = GRPORewardModel(tokenizer, model_with_lora, grpo_batch=12, model_device=device, dtype=dtype)
         
         # generation_ids = grpo_reward_model.generate(input_ids, attention_mask=attention_mask)
         # print(f"Generated text: {tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
