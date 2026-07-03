74fb3fb11412cce6519d874610129fbfb3dfaf76
debashis-das <2009debashis@gmail.com>
2026-07-03 18:36:51 +0530

    Deb || logits usage during sampling

--- src/lora/LoRAFineTuning.py
+++ src/lora/LoRAFineTuning.py
@@ -214,7 +214,7 @@ class LoRAFineTuning(nn.Module):
                 if sampling :
                     # if temperature is None:
                     #     next_token_logits = next_token_logits - next_token_logits.max(dim=-1, keepdim=True).values
-                    # next_token_logits = torch.softmax(next_token_logits, dim=-1)
+                    next_token_logits = torch.softmax(next_token_logits, dim=-1)
                     next_token = torch.distributions.Categorical(logits=next_token_logits).sample((batch,))  # Sample from the distribution
                 else:
                     next_token = next_token_logits.argmax(dim=-1, keepdim=True)  # Greedy decoding
