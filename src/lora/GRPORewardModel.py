f41f6fc663f9794c918d0b0c815da203ac37a328
debashis-das <2009debashis@gmail.com>
2026-07-04 12:23:24 +0530

    Deb || model run in eval mode

--- src/lora/GRPORewardModel.py
+++ src/lora/GRPORewardModel.py
@@ -208,6 +208,7 @@ class GRPORewardModel(nn.Module):
     
     def generate(self, input_ids, attention_mask):
         with torch.no_grad():
+            self.model.eval()
             output_tensor = self.model.generate(input_ids.to(self.model_device), 
                                             attention_mask=attention_mask.to(self.model_device), 
                                             max_new_tokens=self.total_generation_length, 
