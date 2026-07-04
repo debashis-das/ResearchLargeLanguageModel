b35af23af0b78ae3e43d9c4162d2fc4c17fa1269
debashis-das <2009debashis@gmail.com>
2026-07-04 11:55:36 +0530

    Deb || GRPO removed sft parameters from base

--- src/lora/GRPORewardModel.py
+++ src/lora/GRPORewardModel.py
@@ -61,8 +61,7 @@ class GRPORewardModel(nn.Module):
         # for p in self.model.parameters():
         #     p.data += 0.005 * torch.randn_like(p)  
         self.base_model = LoRAFineTuning(init_model, self.tokenizer, device=init_model.device)
-        if loRA_parameters_path:
-            self.base_model.load_lora_parameters(loRA_parameters_path)
+        
         for param in self.base_model.parameters():
             param.requires_grad = False 
 
