7476f0d7b1351d9faf15b2f67aa267245a3cc1fd
debashis-das <2009debashis@gmail.com>
2026-07-04 19:44:42 +0530

    Deb || init generation logs removed

--- src/chess/rl_training.py
+++ src/chess/rl_training.py
@@ -17,21 +17,21 @@ def rl_train(tokenizer_path=None, model_path=None, loRA_parameters_path=None, dt
                                             loRA_parameters_path=loRA_parameters_path)
         device = grpo_reward_model.model_device
         optimizer = torch.optim.AdamW(grpo_reward_model.parameters(), lr=1e-5)
-        paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/000001-rl.parquet"
-        df_input = pd.read_parquet(paraquet)
-        row = df_input.sample(n=1).iloc[0]
-        input_ids = torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
-        attention_mask = torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
-        input_ids = input_ids.repeat_interleave(repeats=5, dim=0)  # Repeat the input tensor for the batch size
-        attention_mask = attention_mask.repeat_interleave(repeats=5, dim=0)  # Repeat the attention mask for the batch size
-        
-        generation_ids = grpo_reward_model.generate(input_ids, attention_mask=attention_mask)
-        print(f"Before passing it to GRPO Generated text: {grpo_reward_model.tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
-        print("----------------------------------------------------------------------------------------------------")
+        # paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/000001-rl.parquet"
+        # df_input = pd.read_parquet(paraquet)
+        # row = df_input.sample(n=1).iloc[0]
+        # input_ids = torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
+        # attention_mask = torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
         # input_ids = input_ids.repeat_interleave(repeats=5, dim=0)  # Repeat the input tensor for the batch size
         # attention_mask = attention_mask.repeat_interleave(repeats=5, dim=0)  # Repeat the attention mask for the batch size
-        generation_ids = grpo_reward_model.generate(input_ids, attention_mask=attention_mask)
-        print(f"Generated text: {grpo_reward_model.tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
+        
+        # generation_ids = grpo_reward_model.generate(input_ids, attention_mask=attention_mask)
+        # print(f"Before passing it to GRPO Generated text: {grpo_reward_model.tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
+        # print("----------------------------------------------------------------------------------------------------")
+        # input_ids = input_ids.repeat_interleave(repeats=5, dim=0)  # Repeat the input tensor for the batch size
+        # attention_mask = attention_mask.repeat_interleave(repeats=5, dim=0)  # Repeat the attention mask for the batch size
+        # generation_ids = grpo_reward_model.generate(input_ids, attention_mask=attention_mask)
+        # print(f"Generated text: {grpo_reward_model.tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
 
         # exit()
         training_timestep = 0
