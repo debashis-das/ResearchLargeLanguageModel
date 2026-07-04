e081094f7b43dd28d51093bb294ac6f0fd50894b
debashis-das <2009debashis@gmail.com>
2026-07-04 11:08:13 +0530

    Deb || rl prompt debug

--- src/chess/rl_training.py
+++ src/chess/rl_training.py
@@ -25,9 +25,9 @@ def rl_train(tokenizer=None, model=None, model_with_lora=None, device=None, dtyp
         
         generation_ids = model_with_lora.generate(input_ids, attention_mask=attention_mask, max_new_tokens=50, temperature=0.7)
         print(f"Before passing it to GRPO Generated text: {tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
-        
-        input_ids = input_ids.repeat_interleave(repeats=5, dim=0)  # Repeat the input tensor for the batch size
-        attention_mask = attention_mask.repeat_interleave(repeats=5, dim=0)  # Repeat the attention mask for the batch size
+        print("----------------------------------------------------------------------------------------------------")
+        # input_ids = input_ids.repeat_interleave(repeats=5, dim=0)  # Repeat the input tensor for the batch size
+        # attention_mask = attention_mask.repeat_interleave(repeats=5, dim=0)  # Repeat the attention mask for the batch size
         grpo_reward_model = GRPORewardModel(tokenizer, model_with_lora, grpo_batch=4, model_device=device, dtype=dtype)
         generation_ids = grpo_reward_model.generate(input_ids, attention_mask=attention_mask)
         print(f"Generated text: {tokenizer.batch_decode(generation_ids, skip_special_tokens=True)}")  
