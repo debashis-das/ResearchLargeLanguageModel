19a00f8f28a37723b257b561918d669fdee9df12
debashis-das <2009debashis@gmail.com>
2026-07-05 19:41:29 +0530

    Deb | grpo batch to 18 and generation tokens to 300

--- src/chess/rl_training.py
+++ src/chess/rl_training.py
@@ -10,7 +10,7 @@ import torch
 
 from lora.GRPORewardModel import GRPORewardModel
 
-GRPO_BATCH_SIZE = 20
+GRPO_BATCH_SIZE = 18
 
 def rl_train(tokenizer_path=None, model_path=None, loRA_parameters_path=None, dtype=None, replay_buffer=None):
     try:
--- src/chess/supervised_training.py
+++ src/chess/supervised_training.py
@@ -45,7 +45,7 @@ def sft_train():
                     row_rl = df_rl_input.sample(n=1).iloc[0]
                     input_ids_rl = torch.tensor(row_rl['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
                     attention_mask_rl = torch.tensor(row_rl['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
-                    generation_ids = model_with_lora.generate(input_ids_rl, max_new_tokens=200, temperature=0.7)
+                    generation_ids = model_with_lora.generate(input_ids_rl, max_new_tokens=300, temperature=0.7)
                     print(f"Generated text: {tokenizer.decode(generation_ids[0], skip_special_tokens=True)}")  # Debugging line to check generated text
                 try:
                     _, loss = model_with_lora(input_ids, attention_mask=attention_mask)
@@ -89,7 +89,7 @@ def sft_execute():
         row_rl = df_rl_input.sample(n=1).iloc[0]
         input_ids_rl = torch.tensor(row_rl['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
         attention_mask_rl = torch.tensor(row_rl['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
-        output_ids = model_with_lora.generate(input_ids_rl, max_new_tokens=200, temperature=0.7)
+        output_ids = model_with_lora.generate(input_ids_rl, max_new_tokens=300, temperature=0.7)
         print(f"Generated text: {tokenizer.decode(output_ids[0], skip_special_tokens=True)}")  # Debugging line to check generated text
 
 if __name__ == "__main__":
