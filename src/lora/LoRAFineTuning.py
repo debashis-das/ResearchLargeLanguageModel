87a5f1b55707a0886031deafb38c4937b7600082
debashis-das <2009debashis@gmail.com>
2026-07-07 23:29:47 +0530

    Deb || lora changes

--- src/lora/LoRAFineTuning.py
+++ src/lora/LoRAFineTuning.py
@@ -116,7 +116,7 @@ class LoRAFineTuning(nn.Module):
         o_mlp = self.module_dict[f"model.layers.{current_layer_number}.mlp"](o_projection_norm)
         return o_mlp + o_projection_residual
         
-    def forward(self, X, attention_mask=None, with_no_loss=False):
+    def forward(self, X, attention_mask=None, prompt_length=None, with_no_loss=False):
         assert len(X.shape) in (1, 2), (
             f"Expected input_ids of shape [seq_len] or [batch, seq_len], got {X.shape}"
         )
@@ -151,7 +151,14 @@ class LoRAFineTuning(nn.Module):
         logits = logits_batch.view(B * S, V)
         X = X.view(B * S)
         shifted_logits = logits[...,:-1,:].contiguous()
-        shifted_labels = X[..., 1:].contiguous()
+        if prompt_length is not None:
+            labels = X.clone()
+            for label in range(batch_size):
+                labels[label, :prompt_length[label]] = -100  # Ignore the last token of the prompt for loss computation
+            labels[labels == self.tokenizer.pad_token_id] = -100  # Ignore padding tokens for loss computation
+            shifted_labels = labels[..., 1:].contiguous()
+        else:
+            shifted_labels = X[..., 1:].contiguous()  # Shift labels for next-token prediction
         # Compute loss
         loss = self.loss_function(shifted_logits, shifted_labels)
         return output_logits, loss
