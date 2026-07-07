300773506611440a763068728761984244c3e4b6
debashis-das <2009debashis@gmail.com>
2026-07-07 21:15:02 +0530

    Deb || supervised training print memeory after every training step

--- src/chess/supervised_training.py
+++ src/chess/supervised_training.py
@@ -77,8 +77,9 @@ def sft_train():
                     print(f"An error occurred during model training: {e}")
                     raise
                 finally:
-                    for i in range(torch.cuda.device_count()):
-                        print(f"[GPU {i}] Allocated: {torch.cuda.memory_allocated(i)/1024**2:.2f} MB, Max Allocated: {torch.cuda.max_memory_allocated(i)/1024**2:.2f} MB, Reserved: {torch.cuda.memory_reserved(i)/1024**2:.2f} MB, Max Reserved: {torch.cuda.max_memory_reserved(i)/1024**2:.2f} MB")
+                    if current_batch == 0:
+                        for i in range(torch.cuda.device_count()):
+                            print(f"[GPU {i}] Allocated: {torch.cuda.memory_allocated(i)/1024**2:.2f} MB, Max Allocated: {torch.cuda.max_memory_allocated(i)/1024**2:.2f} MB, Reserved: {torch.cuda.memory_reserved(i)/1024**2:.2f} MB, Max Reserved: {torch.cuda.max_memory_reserved(i)/1024**2:.2f} MB")
                     if (training_timestep % 500 == 0 and loss is not None) or end:
                         model_with_lora.save_lora_parameters("model/sft_lora_parameters.pt")
                         torch.save({'optimizer_state_dic': optimizer.state_dict(), 'epoch_per_parquet': training_timestep}, 
