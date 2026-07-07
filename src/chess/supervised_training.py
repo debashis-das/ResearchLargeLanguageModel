a6bec267f2033718bfe9a513a53b1f03643713fa
debashis-das <2009debashis@gmail.com>
2026-07-07 21:07:12 +0530

    Deb || save on end

--- src/chess/supervised_training.py
+++ src/chess/supervised_training.py
@@ -36,58 +36,59 @@ def sft_train():
             batch = 14
             current_batch = 0
             for _, row in df_shuffled.iterrows():
-                if training_timestep > 5:
-                    end = True
-                    break
-                if current_batch == 0:
-                    input_ids = torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
-                    attention_mask = torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
-                    current_batch += 1
-                    continue
-                elif current_batch < batch:
-                    input_ids = torch.cat([input_ids, torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)], dim=0)
-                    attention_mask = torch.cat([attention_mask, torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)], dim=0)
-                    current_batch += 1
-                    continue
-                else:
-                    current_batch = 0
-                    if training_timestep % 10 == 0:
-                        current_rl_paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/{i:06d}-rl.parquet"
-                        df_rl_input = pd.read_parquet(current_rl_paraquet)
-                        row_rl = df_rl_input.sample(n=1).iloc[0]
-                        input_ids_rl = torch.tensor(row_rl['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
-                        attention_mask_rl = torch.tensor(row_rl['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
-                        generation_ids = model_with_lora.generate(input_ids_rl, max_new_tokens=300, temperature=0.7)
-                        print(f"Generated text: {tokenizer.decode(generation_ids[0], skip_special_tokens=True)}")  # Debugging line to check generated text
-                    try:
-                        _, loss = model_with_lora(input_ids, attention_mask=attention_mask)
-                        # loss = loss / accumulation_steps
-                        loss.backward()
-                        # running_loss += loss.item()*accumulation_steps
-                        training_timestep += 1
-                        # if training_timestep % accumulation_steps == 0:
-                        optimizer.step()
-                        optimizer.zero_grad(set_to_none=True)
-                        print(f"Training timestep: {training_timestep}, Loss: {loss.item()}")
-                        # if running_loss/accumulation_steps < 0.78:
-                        #     print(f"Loss is very low, stopping training at timestep: {training_timestep}, Loss: {running_loss/accumulation_steps}")
-                        #     return
-                        # running_loss = torch.zeros([1], dtype=torch.float32, device=device)
-                    except Exception as e:
-                        print(f"An error occurred during model training: {e}")
-                        raise
-                    finally:
-                        for i in range(torch.cuda.device_count()):
-                            print(f"[GPU {i}] Allocated: {torch.cuda.memory_allocated(i)/1024**2:.2f} MB, Max Allocated: {torch.cuda.max_memory_allocated(i)/1024**2:.2f} MB, Reserved: {torch.cuda.memory_reserved(i)/1024**2:.2f} MB, Max Reserved: {torch.cuda.max_memory_reserved(i)/1024**2:.2f} MB")
-                        if (training_timestep % 500 == 0 and loss is not None) or end:
-                            model_with_lora.save_lora_parameters("model/sft_lora_parameters.pt")
-                            torch.save({'optimizer_state_dic': optimizer.state_dict(), 'epoch_per_parquet': training_timestep}, 
-                                    f"model/sft_optimizer_with_timestep_state_dict.pt")
-                            print(f"Model training complete saved")
-                        del input_ids
-                        del attention_mask
-                        gc.collect()
-                        torch.cuda.empty_cache()
+                try:
+
+                    if training_timestep > 5:
+                        end = True
+                        break
+                    if current_batch == 0:
+                        input_ids = torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
+                        attention_mask = torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
+                        current_batch += 1
+                        continue
+                    elif current_batch < batch:
+                        input_ids = torch.cat([input_ids, torch.tensor(row['input_ids'], dtype=torch.long, device=device).unsqueeze(0)], dim=0)
+                        attention_mask = torch.cat([attention_mask, torch.tensor(row['attention_mask'], dtype=dtype, device=device).unsqueeze(0)], dim=0)
+                        current_batch += 1
+                        continue
+                    else:
+                        current_batch = 0
+                        if training_timestep % 10 == 0:
+                            current_rl_paraquet = f"/home/ResearchLargeLanguageModel/src/chess/paraquets/{i:06d}-rl.parquet"
+                            df_rl_input = pd.read_parquet(current_rl_paraquet)
+                            row_rl = df_rl_input.sample(n=1).iloc[0]
+                            input_ids_rl = torch.tensor(row_rl['input_ids'], dtype=torch.long, device=device).unsqueeze(0)
+                            attention_mask_rl = torch.tensor(row_rl['attention_mask'], dtype=dtype, device=device).unsqueeze(0)
+                            generation_ids = model_with_lora.generate(input_ids_rl, max_new_tokens=300, temperature=0.7)
+                            print(f"Generated text: {tokenizer.decode(generation_ids[0], skip_special_tokens=True)}")  # Debugging line to check generated text
+                            _, loss = model_with_lora(input_ids, attention_mask=attention_mask)
+                            # loss = loss / accumulation_steps
+                            loss.backward()
+                            # running_loss += loss.item()*accumulation_steps
+                            training_timestep += 1
+                            # if training_timestep % accumulation_steps == 0:
+                            optimizer.step()
+                            optimizer.zero_grad(set_to_none=True)
+                            print(f"Training timestep: {training_timestep}, Loss: {loss.item()}")
+                            # if running_loss/accumulation_steps < 0.78:
+                            #     print(f"Loss is very low, stopping training at timestep: {training_timestep}, Loss: {running_loss/accumulation_steps}")
+                            #     return
+                            # running_loss = torch.zeros([1], dtype=torch.float32, device=device)
+                except Exception as e:
+                    print(f"An error occurred during model training: {e}")
+                    raise
+                finally:
+                    for i in range(torch.cuda.device_count()):
+                        print(f"[GPU {i}] Allocated: {torch.cuda.memory_allocated(i)/1024**2:.2f} MB, Max Allocated: {torch.cuda.max_memory_allocated(i)/1024**2:.2f} MB, Reserved: {torch.cuda.memory_reserved(i)/1024**2:.2f} MB, Max Reserved: {torch.cuda.max_memory_reserved(i)/1024**2:.2f} MB")
+                    if (training_timestep % 500 == 0 and loss is not None) or end:
+                        model_with_lora.save_lora_parameters("model/sft_lora_parameters.pt")
+                        torch.save({'optimizer_state_dic': optimizer.state_dict(), 'epoch_per_parquet': training_timestep}, 
+                                f"model/sft_optimizer_with_timestep_state_dict.pt")
+                        print(f"Model training complete saved")
+                    del input_ids
+                    del attention_mask
+                    gc.collect()
+                    torch.cuda.empty_cache()
     except Exception as e:
         print(f"An error occurred during training the model: {e}")
         traceback.print_exc()
